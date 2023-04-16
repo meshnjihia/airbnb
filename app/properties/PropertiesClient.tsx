326d9bee8fbc06c7fc3c16f01b2d4a66198f14e2
meshnjihia <meshworks7@gmail.com>
2023-04-17 02:47:00 +0300

    created properties page

---
+++ app/properties/PropertiesClient.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { toast } from 'react-hot-toast'
+import axios from 'axios'
+import { useCallback, useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+import Heading from '@/app/components/Heading'
+import Container from '@/app/components/Container'
+import ListingCard from '@/app/components/listings/ListingCard'
+import { SafeListing, SafeUser } from '@/types'
+
+interface PropertiesClientProps {
+  listings: SafeListing[]
+  currentUser?: SafeUser | null
+}
+
+const PropertiesClient: React.FC<PropertiesClientProps> = ({
+  listings,
+  currentUser,
+}) => {
+  const router = useRouter()
+  const [deletingId, setDeletingId] = useState('')
+
+  const onDelete = useCallback(
+    (id: string) => {
+      setDeletingId(id)
+
+      axios
+        .delete(`/api/listings/${id}`)
+        .then(() => {
+          toast.success('property listing deleted')
+          router.refresh()
+        })
+        .catch((error) => {
+          toast.error('something went wrong!')
+        })
+        .finally(() => {
+          setDeletingId('')
+        })
+    },
+    [router],
+  )
+
+  return (
+    <Container>
+      <Heading title="Properties" subtitle="List of your properties" />
+      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing: any) => (
+          <ListingCard
+            key={listing.id}
+            data={listing}
+            actionId={listing.id}
+            onAction={onDelete}
+            disabled={deletingId === listing.id}
+            actionLabel="Delete property"
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  )
+}
+
+export default PropertiesClient
