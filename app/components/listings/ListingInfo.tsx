05626e5e09da61ddaacc3e38e4998960f365b9db
meshnjihia <meshworks7@gmail.com>
2023-04-16 06:18:22 +0300

    more listing information

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -1,13 +1,30 @@
-import { SafeUser } from '@/types'
+'use client'
 import React from 'react'
+import dynamic from 'next/dynamic';
+
+import { IconType } from 'react-icons';
+
+import { SafeUser } from '@/types'
+import useCountries from '@/app/hooks/useCountries';
+
+import Avatar from '../Avatar';
+import ListingCategory from './ListingCategory';
+
+const Map = dynamic(() => import('../Map'), {
+  ssr: false
+})
 
 interface ListingInfoProps{
   user: SafeUser;
-  category: string
   description: string
-  roomCount: number
   guestCount: number
+  roomCount: number
   bathroomCount: number
+  category: {
+    icon: IconType
+    label: string
+    description: string
+  } | undefined
   locationValue: string
 }
 
@@ -20,8 +37,44 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
   bathroomCount,
   locationValue
 }) => {
+  const { getByValue } = useCountries();
+  const coordinates = getByValue(locationValue)?.latlng;
   return (
-    <div>ListingInfo</div>
+    <div className='col-span-4 flex flex-col gap-8'>
+      <div className='flex flex-col gap-2'>
+        <div className='text-xl font-semibold flex flex-row items-center gap-2'>
+          <div>
+            Hosted by {user?.name}
+          </div>
+          <Avatar src={user?.image}/>
+        </div>
+        <div className='flex flex-row gap-4 items-center font-light text-neutral-500'>
+          <div>
+            {guestCount} Guests
+          </div>
+          <div>
+            {roomCount} Rooms
+          </div>
+          <div>
+            {bathroomCount} Bathrooms
+          </div>
+        </div>
+      </div>
+      <hr />
+      {category && (
+        <ListingCategory
+          icon={category.icon}
+          label={category.label}
+          description={category.description}
+        />
+      )}
+      <hr />
+      <div className='font-light text-lg text-neutral-500'>
+        {description}
+      </div>
+      <hr />
+      <Map center={coordinates}/>
+    </div>
   )
 }
 
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -35,7 +35,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
           <div className='grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6'>
             <ListingInfo
               user={listing.user}
-              category={listing.category}
+              category={category}
               description={listing.description}
               roomCount={listing.roomCount}
               guestCount={listing.guestCount}
