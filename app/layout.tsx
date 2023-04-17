dbdb606ab6be53cd66d53d99b834448668684e60
meshnjihia <meshworks7@gmail.com>
2023-04-17 03:14:44 +0300

    created search modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import './globals.css'
 import { Nunito } from 'next/font/google'
 import ToasterProvider from './providers/ToasterProvider'
 import getCurrentUser from './actions/getCurrentUser'
+import SearchModal from './components/modals/SearchModal'
 
 export const metadata = {
   title: 'airbnb clone',
@@ -30,6 +31,7 @@ export default async function RootLayout({
           <ToasterProvider />
           <RegisterModal />
           <LoginModal />
+          <SearchModal />
           <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
