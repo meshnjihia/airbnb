e7d8c34d26ce1d38c456d4dbdf8081078a6b22a7
meshnjihia <meshworks7@gmail.com>
2023-05-22 11:10:19 +0300

    react

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Container from "@/app/components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 import EmptyState from "@/app/components/EmptyState";
-
+import React from "react";
 import getListings, { IListingsParams } from "@/app/actions/getListings";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
